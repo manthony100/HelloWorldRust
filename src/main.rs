6fe92fd64360c3b8d6b12bdbf47b5bbe9c611a06
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2026-09-21 16:22:50 -0400

    Bitwise operations

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,29 @@ fn main() {
     println!("c is {:08.3}\na is {}", c, a);
     println!("c is {0:08.3}\na is {1}\nonce again, c is {0}", c, a);
 
+    let mut value = 0b1111_0101u8; // binary literal
+    println!("value is {}", value); // prints the binary representation of value
+    println!("value is {:08b}", value); // prints the octal representation of value
+
+    value = !value; // bitwise NOT operator, flips all bits in value
+    println!("value is {:08b}", value); // prints the octal representation of value
+
+    value = value & 0b1111_0111; // bitwise AND operator, sets value to the result of ANDing value with 0b1111_0111
+    println!("AND: value is {:08b}", value); // prints the octal representation of value
+    println!("bit 6 is {}", value & 0b0100_0000); // prints the value of bit 6 - bitwise AND operator, checks if bit 6 is set
+
+    value = value | 0b0100_0000; // bitwise OR operator, sets value to the result of ORing value with 0b0000_1000
+    println!("OR: value is {:08b}", value); // prints the octal representation of value 
+
+
+    value = value ^ 0b0101_0101; // bitwise XOR operator, sets value to the result of XORing value with 0b0101_0101
+    println!("XOR: value is {:08b}", value); // prints the octal representation of value
+
+    // Bit shifting operators
+    value = value << 4; // left shift operator, shifts all bits in value to the left by 4 positions
+    println!("Left shift: value is {:08b}", value); // prints the octal representation of value
+    
+    value = value >> 2; // right shift operator, shifts all bits in value to the right by 2 positions
+    println!("Right shift: value is {:08b}", value); // prints the octal representation of value
 
 }
