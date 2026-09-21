af7fcfa512580f7c6fdc6e1ec6e0066f8a103acb
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2026-09-21 15:31:20 -0400

    Beginning of linkedin learning, chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    /* multiple 
+    line 
+    comment
+     */
+    let mut x = 10;
+    println!("x is {}", x);
+    // have to use mut to make x mutable
+
+    x = 20; // This will cause a compile-time error because x is immutable
+    println!("x is {}", x);
 }
