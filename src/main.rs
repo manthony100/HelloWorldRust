3f47ad120c3c8f1f2fa6f7328ee4659846dec280
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2026-09-21 15:58:19 -0400

    Formatting print statements

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ fn main() {
 
     x = 20; // This will cause a compile-time error because x is immutable
     println!("x is {}", x);
-    let a = 10;
+    let a = 10.0;
     let b = 3.0;
-    let c = a as f64 / b; // before a and b were decimals it truncates the decimal part and returns 3
+    let c = a / b; // before a and b were decimals it truncates the decimal part and returns 3
     // casting from integer to float is usually fine since data isn't lost, but casting from float to integer can lose data
-    println!("c is {}", c);
+    println!("c is {:08.3}\na is {}", c, a);
+    println!("c is {0:08.3}\na is {1}\nonce again, c is {0}", c, a);
+
+
 }
