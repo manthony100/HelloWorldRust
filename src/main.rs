04ec395f375107bb69d8c82152e94ae0b6cd1c5d
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2026-09-21 15:51:34 -0400

    Videos 1-4 of chapter 2

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,9 @@ fn main() {
 
     x = 20; // This will cause a compile-time error because x is immutable
     println!("x is {}", x);
+    let a = 10;
+    let b = 3.0;
+    let c = a as f64 / b; // before a and b were decimals it truncates the decimal part and returns 3
+    // casting from integer to float is usually fine since data isn't lost, but casting from float to integer can lose data
+    println!("c is {}", c);
 }
