1401187f4378bfffbe6df3aa6be35df4cb7e8ad7
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2026-09-22 15:23:16 -0400

    Nested loops

--- src/main.rs
+++ src/main.rs
@@ -168,16 +168,27 @@ fn main() {
 
     // }
 
-    let message = ['h', 'e', 'l', 'l', 'o'];
-    for (index, &item) in message.iter().enumerate() {
-        println!("item is {}", item);
-        if item == 'e' {
-            break;
-        }
-    }
+    // let message = ['h', 'e', 'l', 'l', 'o'];
+    // for (index, &item) in message.iter().enumerate() {
+    //     println!("item is {}", item);
+    //     if item == 'e' {
+    //         break;
+    //     }
+    // }
 
-    for nummber in 0..5 {
-        println!("number is {}", nummber);
+    // for nummber in 0..5 {
+    //     println!("number is {}", nummber);
+    // }
+
+    let mut matrix = [[1, 2, 3], 
+                                [4, 5, 6],
+                                [7, 8, 9]];
+    for row in matrix.iter_mut(){
+        for num in row.iter_mut(){
+            *num += 10;
+            print!("{}\t", num);
+        }
+        println!();
     }
     
 }
