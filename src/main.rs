6a525ea0c4bce9fa824d4c26fa1f222dc166710e
Mars Anthony <145798641+manthony100@users.noreply.github.com>
2026-09-22 00:19:44 -0400

    loops

--- src/main.rs
+++ src/main.rs
@@ -137,16 +137,28 @@ fn main() {
     //     println!("x is equal to y");
     // }
 
-    let make_x_odd = true;
-    let x if make_x_odd { 3 } else { 2 };
+    // let make_x_odd = true;
+    // let x if make_x_odd { 3 } else { 2 };
 
-    if make_x_odd {
-        x = 3;
-    } else {
-        x = 2;
-    }
+    // if make_x_odd {
+    //     x = 3;
+    // } else {
+    //     x = 2;
+    // }
 
-    println!("x is {}", x);
+    // println!("x is {}", x);
+
+    let mut count = 0;
+    let result = loop {
+        if count == 10 {
+            break count * 10;
+        }
+        count += 1;
+        println!("count is {}", count);
+    };
+
+    println!("After the loop!");
+    println!("result is {}", result);
 
 }
 fn celsious_to_fahrenheit(celsius: f64) -> f64 {
